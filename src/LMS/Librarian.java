d22531eae5fd0820424e97fa7efd2ca69c2aa7fc
Salma <selma.1390@gmail.com>
2024-10-08 11:04:09 -0400

    Added methods of Book class to the Library class and called them into the Librarian which is our main class. - By Salma

--- src/LMS/Librarian.java
+++ src/LMS/Librarian.java
@@ -4,8 +4,24 @@ public class Librarian {
 
     public static void main(String[] args) {
 
-        Book obj = new Book(1111,"Asdf", "Aaaa", "JIKK",121,true);
+        Book book1 = new Book("1234-1", "FirstBook", "FirstBookAuthor", "Bob", 2000, true);
+        Book book2 = new Book("1234-2", "SecondBook", "SecondBookAuthor", "Tony", 2001, true);
+        Book book3 = new Book("1234-3", "ThirdBook", "ThirdBookAuthor", "Morphy", 2002, true);
 
-        System.out.println( obj.toString());
+
+
+        System.out.println(book1.toString());
+
+        // the below methods from Library class called - Salma
+        Library library = new Library();
+
+        library.insertBook(book1);
+        library.insertBook(book2);
+        library.insertBook(book3);
+        library.displayAllBooks();
+        library.searchBookByISBN("1234-1");
+        library.deleteBookByISBN("1234-1");
+
+
     }
 }
--- src/LMS/Library.java
+++ src/LMS/Library.java
@@ -7,34 +7,31 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.*;
 
-public class Library{
+public class Library {
 
     private List<Book> booksList;
     private List<Member> membersList;
     private List<Transactions> transactionsList;
-    private Map<Integer, Book> bookSearch;
+    private Map<Integer, Book> bookSearch; // No need for this since we can search only by ISBN and that is from type of String
     private Map<Integer, Member> memberSearch;
     private BufferedWriter addLog;
     private BufferedWriter removeLog;
     public int memberCounter;
 
-    public Library() throws IOException {
-     booksList = new ArrayList<>();
-     membersList = new ArrayList<>();
-     transactionsList = new ArrayList<>();
-     bookSearch = new HashMap<>();
-     memberSearch = new HashMap<>();
-     addLog = new BufferedWriter(new FileWriter("Add Log.txt"));
-     removeLog = new BufferedWriter(new FileWriter("Remove Log.txt"));
+    public Library() {
+        booksList = new ArrayList<>();
+        membersList = new ArrayList<>();
+        transactionsList = new ArrayList<>();
+        bookSearch = new HashMap<>();
+        memberSearch = new HashMap<>(); // no need to have this
+
+
     }
 
-    public void addMember(int memberID, String name, String email, String phoneNumber, ArrayList<String> borrowedBooks ) throws IOException {
-        membersList.add(new Member(memberID,name,email,phoneNumber,borrowedBooks));
+    public void addMember(int memberID, String name, String email, String phoneNumber, ArrayList<String> borrowedBooks) throws IOException {
+        membersList.add(new Member(memberID, name, email, phoneNumber, borrowedBooks));
         addLog.write("\n");
-        addLog.write(memberCounter++ +": " + memberID + ", "
-                + name + ", "
-                + email + ", "
-                + phoneNumber + ", " + borrowedBooks.get(memberCounter));
+        addLog.write(memberCounter++ + ": " + memberID + ", " + name + ", " + email + ", " + phoneNumber + ", " + borrowedBooks.get(memberCounter));
         addLog.close();
     }
 
@@ -48,10 +45,72 @@ public class Library{
         memberCounter--;
     }
 
+    /*
+     * The methods below belong to the Book class, created by Salma.
+     * If you have any comments or suggestions, please feel free to share them.
+     * As we know, essential methods for the Book class include adding a book, removing a book,
+     * searching for a book, and listing all books.
+     */
 
 
+    /* insertBook Method
+    It takes a single parameter, newBook, which is an instance of the Book class. This parameter represents the book that will be added to
+    the library.
+    The insertBook method efficiently adds a new Book object to the library's collection and informs the user of the successful addition
+     by displaying the book's title.
+     */
+    public void insertBook(Book newBook) {
+        booksList.add(newBook);
+        System.out.println("One new book added: " + newBook.getTitle() + " by " + newBook.getAuthor() + ".");
+    }
 
 
+    /*
+    The deleteBookByISBN method calls another method searchBookByISBN(isbn), which searches the library's book collection (booksList) for a book that
+     matches the provided ISBN.
+     If a matching book is found, it is assigned to the variable bookToRemove. If no book is found, bookToRemove will be null.
+     */
+
+    public void deleteBookByISBN(String isbn) {
+        Book bookToRemove = searchBookByISBN(isbn);
+        if (bookToRemove != null) {
+            booksList.remove(bookToRemove);
+            System.out.println("Book removed: " + bookToRemove.getTitle() + " by " + bookToRemove.getAuthor() + ".");
+        } else {
+            System.out.println("Book with ISBN " + isbn + " not found.");
+        }
+
+    }
+
+
+    /*
+    This searchBookByISBN method is defined to return a Book object, indicating that it either returns the found book or null if no book is found.
+It takes a single parameter isbn, which is a String representing the ISBN of the book to search for.
+     */
+    Book searchBookByISBN(String isbn) {
+        for (Book book : booksList) {
+            if (book.getISBN().equals(isbn)) {
+                return book;
+            }
+        }
+        return null;
+    }
+
+    /*
+    The displayAllBooks method is designed to display all the books in the library's collection. It checks if there are any books present
+    and, if so, iterates through the list of books to print out their details.
+     */
+
+    public void displayAllBooks() {
+        if (booksList.isEmpty()) {
+            System.out.println("No books in the library.");
+        } else {
+            for (Book book : booksList) {
+                System.out.println(book.toString());
+                System.out.println("****** ****** ****** ******");
+            }
+        }
+    }
 
 
 }
