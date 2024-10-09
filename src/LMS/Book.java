126dd6033513e4be5195a6c789cdaec29db49200
hassanfadavi <s.hasanfadavi@gmail.com>
2024-10-09 11:14:15 -0400

    book class has been updated

--- src/LMS/Book.java
+++ src/LMS/Book.java
@@ -63,12 +63,13 @@ public class Book {
         this.inventory++;
     }
 
-     //if statement is written regarding to check the inventory if  is greater than zero
+     //if  statement is written regarding to check the inventory if  is greater than zero
     public void decreaseInventory(){
         if(this.inventory>0) {
             this.inventory--;
             if (this.inventory == 0) {
                 this.isAvailable = false;
+
             }
         }
     }
